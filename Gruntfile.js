6425d4545233fdbcbe46b27ae139d33ccf3b274d
neelxie <kidricederek@gmail.com>
2021-10-18 17:02:37 +0300

    Add necessary plugind

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    }
+  });
+  grunt.loadNPMTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('')
+  }
